4676b14d6f6ad8c428cbeb0b72774c6c916f78ee
Rolandas <rolarola631@gmail.com>
2024-07-04 18:02:02 +0300

    added auth

--- 2. back-end/models/Booking.js
+++ 2. back-end/models/Booking.js
@@ -1,42 +1,37 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
 
 const bookingSchema = new mongoose.Schema({
-    businessId: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
+  businessId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  time: {
+    type: String,
+    required: true,
+  },
+  userEmail: {
+    type: String,
+    required: true,
+    validate: [isEmail, 'Please enter a valid email'],
+  },
+  userName: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: [true, 'Booking status is required.'],
+    enum: {
+      values: ['confirmed', 'pending', 'cancelled'],
+      message: '{VALUE} is not supported',
     },
-    date: {
-        type: Date,
-        required: true,
-    },
-    time: {
-        type: String,
-        required: true,
-    },
-    userEmail: {
-        type: String,
-        required: true,
-        validate: {
-            validator: function (email) {
-                return /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(
-                    email
-                );
-            },
-            message: (props) => `${props.value} is not a valid email!`,
-        },
-    },
-    userName: {
-        type: String,
-        required: true,
-    },
-    status: {
-        type: String,
-        required: [true, 'Booking status is required.'],
-        enum: {
-            values: ['confirmed', 'pending', 'cancelled'],
-            message: '{VALUE} is not supported',
-        },
-    },
+  },
 });
 
-module.exports = mongoose.model('Booking', bookingSchema);
+const Booking = mongoose.model('Booking', bookingSchema);
+module.exports = Booking;
--- 2. back-end/models/Bussines.js
+++ 2. back-end/models/Bussines.js
@@ -2,40 +2,41 @@ const mongoose = require('mongoose');
 const { isEmail } = require('validator');
 
 const bussinesSchema = new mongoose.Schema({
-    name: {
+  name: {
+    type: String,
+    required: true,
+  },
+  about: {
+    type: String,
+    default: '',
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  contactPerson: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please enter the email adress'],
+    unique: true,
+    validate: [isEmail, 'Please enter a valid email'],
+  },
+  images: [
+    {
+      url: {
         type: String,
         required: true,
+      },
     },
-    about: {
-        type: String,
-        default: '',
-    },
-    address: {
-        type: String,
-        required: true,
-    },
-    category: {
-        type: String,
-        required: true,
-    },
-    contactPerson: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: [true, 'Please enter the email adress'],
-        unique: true,
-        validate: [isEmail, 'Please enter a valid email'],
-    },
-    images: [
-        {
-            url: {
-                type: String,
-                required: true,
-            },
-        },
-    ],
+  ],
 });
 
-module.exports = mongoose.model('Bussines', bussinesSchema);
+const Business = mongoose.model('Bussines', bussinesSchema);
+module.exports = Business;
