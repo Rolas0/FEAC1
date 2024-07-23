74f89b8589b41327a2ee8552beb0e207b4b0c5e8
Rolandas <rolarola631@gmail.com>
2024-07-23 21:50:43 +0300

    Add files for deploy

--- 2. back-end/src/index.ts
+++ 2. back-end/src/index.ts
@@ -3,13 +3,20 @@ import express from 'express';
 import cors from 'cors';
 import routes from './routes/router';
 import { connectToDb, PORT } from './db';
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 app.use(express.json());
 app.use(cors());
 app.use('/', routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../frontend/dist', 'index.html'));
+});
 
 connectToDb()
   .then(() => {
